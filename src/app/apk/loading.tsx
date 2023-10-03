3957caa1cee1e052e15285db862fc791e21c444a
dough <viktor.melin@me.com>
2023-10-03 20:25:26 +0200

    suspense loading

---
+++ src/app/apk/loading.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const loading = () => {
+  return (
+    <svg
+      viewBox='0 0 24 24'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+      aria-live='polite'
+      aria-busy='true'
+      aria-labelledby='title-08a desc-08a'
+      className='w-16 h-16'
+    >
+      <title id='title-08a'>Icon title</title>
+      <desc id='desc-08a'>Some desc</desc>
+      <path d='M7 8H3V16H7V8Z' className='fill-emerald-500 animate animate-bounce ' />
+      <path d='M14 8H10V16H14V8Z' className='fill-emerald-500 animate animate-bounce  [animation-delay:.2s]' />
+      <path d='M21 8H17V16H21V8Z' className='fill-emerald-500 animate animate-bounce  [animation-delay:.4s]' />
+    </svg>
+  );
+};
+
+export default loading;
