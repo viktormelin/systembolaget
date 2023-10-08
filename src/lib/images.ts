a1cbca4bf468f22bc05f1ca3e4dce1edcb466f69
dough-land <viktor.melin@me.com>
2023-10-08 21:09:32 +0200

    add better image quality

--- src/lib/images.ts
+++ src/lib/images.ts
@@ -1,6 +1,6 @@
 export const imageUrl = (images: any) => {
   if (images[0]) {
-    return `${images[0].imageUrl}_100.png`;
+    return `${images[0].imageUrl}_400.png?q=75&w=2000.png`;
   } else {
     return '/no_image.png';
   }
