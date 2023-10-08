21aacead6524fe57b1d7ad28db93d5325e95faa4
dough-land <viktor.melin@me.com>
2023-10-08 21:32:52 +0200

    footer added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css';
-import type { Metadata } from 'next';
 import { Abril_Fatface, Poppins } from 'next/font/google';
 import Navbar from './components/Navbar';
+import Link from 'next/link';
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -51,7 +51,21 @@ export const metadata = {
   // manifest: 'https://panel.dixxel.io/site.webmanifest',
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+const fetchLatestDataCommit = async () => {
+  const response = await fetch(
+    'https://api.github.com/repos/viktormelin/systembolaget/commits?path=data/products.json'
+  );
+  const data = await response.json();
+  if (data) {
+    return data[0].commit.author.date;
+  }
+};
+
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
+  const latestDataCommit = new Date(await fetchLatestDataCommit());
+
+  console.log(latestDataCommit);
+
   return (
     <html lang='en'>
       <body className={poppins.className}>
@@ -59,6 +73,24 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <main className='flex-1 flex flex-col bg-[var(--brand-dark)] relative mx-auto max-w-full px-6 lg:max-w-5xl xl:max-w-7xl 2xl:max-w-[96rem]'>
           {children}
         </main>
+        <footer className='fixed bottom-0 w-full flex justify-center items-center gap-10 p-2'>
+          <p className='text-xs text-gray-500'>
+            Produktdata hämtad från{' '}
+            <Link className='text-xs text-gray-500 hover:underline' href='https://systembolaget.se'>
+              Systembolaget
+            </Link>{' '}
+            {latestDataCommit.toLocaleString('sv-SE', { timeZone: 'Europe/Stockholm' })}
+          </p>
+          <Link
+            className='uppercase text-xs text-gray-500 hover:underline'
+            href='https://github.com/viktormelin/systembolaget'
+          >
+            Github
+          </Link>
+          <Link className='uppercase text-xs text-gray-500 hover:underline' href='https://viktormelin.com'>
+            Kontakt
+          </Link>
+        </footer>
       </body>
     </html>
   );
