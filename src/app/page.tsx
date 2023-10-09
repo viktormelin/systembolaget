099a7a299bc7d2983a1ebf90caa36d6f592611c8
dough-land <viktor.melin@me.com>
2023-10-09 13:55:40 +0200

    update loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,6 @@
-import SearchResult from './components/SearchResult';
-import SearchBar from './components/SearchBar';
-import { Product } from '@/types/Product';
-import { promises, readFile, readFileSync } from 'fs';
-
-// fetch('http://localhost:3000/api/products', { method: 'GET' });
-
-const Home = async (searchParams: { searchParams: { query: string } }) => {
-  const query = searchParams.searchParams['query'];
-  const file = await promises.readFile('data/products.json', 'utf8');
-  const file2 = await promises.readFile('data/countries.json', 'utf8');
-  const products: Product[] = JSON.parse(file);
-  const { countries } = JSON.parse(file2);
-
-  const filteredProducts = products.filter((p) => {
-    return (
-      p.productNameBold?.toLowerCase().includes(query?.toLowerCase()) ||
-      p.productNameThin?.toLowerCase().includes(query?.toLowerCase())
-    );
-  });
-
+const Home = async () => {
   return (
-    <>
-      <SearchBar />
-      {filteredProducts && filteredProducts.length > 0 ? (
-        <SearchResult products={filteredProducts} countries={countries} />
-      ) : null}
-    </>
+    <div className='fixed top-0 left-0 w-screen h-full bg-[url("/aleksandr-popov-2GNBoMgKYEo-unsplash.jpg")] bg-center bg-cover'></div>
   );
 };
 
